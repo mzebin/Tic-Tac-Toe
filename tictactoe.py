7f544492f2a317517e7b666cfa8db6cba8d4a4c9
Mohammed Zebin <mohammed.zebin7@gmail.com>
2021-02-06 17:03:27 +0400

    modified:  tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,7 @@
 # Importing Required Libraries
+import copy
 import os
+import platform
 import random
 import time
 
@@ -8,11 +10,31 @@ class SinglePlayer:
     pass
 
 
+class Easy(SinglePlayer):
+    pass
+
+
+class Intermediate(SinglePlayer):
+    pass
+
+
+class Advanced(SinglePlayer):
+    pass
+
+
 class MultiPlayer:
     pass
 
 
 class Board:
+    pass
+
+
+def clear_screen():
+    pass
+
+
+def get_num_players():
     pass
 
 
